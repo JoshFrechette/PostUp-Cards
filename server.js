17aecdebfb3f138ae25285f5a2af42ab8d515c14
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-15 17:48:32 -0500

    more

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 
-// const express = require("express");
-// // const path = require("path");
+// const path = require("path");
 // const exphbs = require("express-handlebars");
-// const app = express();
-// const PORT = process.env.PORT || 8000;
 
 //-- vvv PROBLEM AREA vvv -- //
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
-// // app.use(express.static("public"));
+// app.use(express.static("public"));
 // app.set("view engine", "handlebars");
 // app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 //-- ^^^ PROBLEM AREA ^^^ -- //
@@ -17,26 +14,24 @@
 // app.get("/", function(req, res) {
 //   res.render("./views.index", {title: 'PostUp Cards'});
 // });
-// app.listen(PORT, function() {
-//   console.log("App listening on PORT " + `http://localhost:${PORT}`);
-// });
 
 var express = require('express');
+var exphbs = require('express-handlebars')
 var app = express();
 var PORT = process.env.PORT || 8000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('view engine', 'exphbs');
+app.engine('exphbs', exphbs({
+  defaultLayout: "main"
+}))
+
 app.get('/', function(req, res) {
-  res.send("Hello World\n")
+  res.render('index', {layout: 'main'})
 })
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + `http://localhost:${PORT}`);
 });
-
-//--- THIS WORKS AT LEAST---//
-// var http = require('http')
-
-// http.createServer(function (request, response) {
-//     response.writeHead(200, {"Content-Type": "text/plain"})
-//     response.end("Hello World\n")
-// }).listen(process.env.PORT)
