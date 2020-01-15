03d6cf59df8b1813d345dc9d8117940944007955
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-15 16:39:45 -0500

    upated server.js file

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 
-// placeholder
-var express = require("express");
-var path = require("path");
-// Set Handlebars.
+const express = require("express");
+const path = require("path");
 const exphbs = require("express-handlebars");
 
-// Sets up the Express App
-var app = express();
-var PORT = process.env.PORT || 8000;
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory to be served
 // app.use(express.static("public"));
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+
 app.set("view engine", "handlebars");
-// Routes
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 
 app.get("/", function(req, res) {
-  res.render(path.join(__dirname, "./views/index"));
+  res.render(path.join(__dirname, "/views.index"), {title: 'PostUp Cards'});
 });
 
-// Starts the server to begin listening
 app.listen(PORT, function() {
   console.log("App listening on PORT " + `http://localhost:${PORT}`);
 });
