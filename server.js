739075439bc6b86b4bb0ae34ff7a6ed51c142537
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-15 16:28:02 -0500

    removed static folder

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory to be served
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 // Routes
