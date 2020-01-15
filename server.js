0caff416c187862bdabf7f5ca1b9250eb8a29642
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-15 17:42:05 -0500

    more

--- server.js
+++ server.js
@@ -2,32 +2,36 @@
 // const express = require("express");
 // // const path = require("path");
 // const exphbs = require("express-handlebars");
-
 // const app = express();
 // const PORT = process.env.PORT || 8000;
 
+//-- vvv PROBLEM AREA vvv -- //
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
 // // app.use(express.static("public"));
-
 // app.set("view engine", "handlebars");
 // app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+//-- ^^^ PROBLEM AREA ^^^ -- //
 
 // app.get("/", function(req, res) {
 //   res.render("./views.index", {title: 'PostUp Cards'});
 // });
-
 // app.listen(PORT, function() {
 //   console.log("App listening on PORT " + `http://localhost:${PORT}`);
 // });
 
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8000;
 
 app.get('/', function(req, res) {
   res.send("Hello World\n")
 })
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + `http://localhost:${PORT}`);
+});
 
 //--- THIS WORKS AT LEAST---//
 // var http = require('http')
