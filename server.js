1ac604cdc0a749652bac0a0586f0a4dce5f3b0f8
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-15 17:30:58 -0500

    take three

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 
-const express = require("express");
-const path = require("path");
-const exphbs = require("express-handlebars");
+// const express = require("express");
+// // const path = require("path");
+// const exphbs = require("express-handlebars");
 
-const app = express();
-const PORT = process.env.PORT || 8000;
+// const app = express();
+// const PORT = process.env.PORT || 8000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// app.use(express.static("public"));
+// // app.use(express.static("public"));
 
-app.set("view engine", "handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+// app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 
-app.get("/", function(req, res) {
-  res.render("./views.index", {title: 'PostUp Cards'});
-});
+// app.get("/", function(req, res) {
+//   res.render("./views.index", {title: 'PostUp Cards'});
+// });
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + `http://localhost:${PORT}`);
-});
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + `http://localhost:${PORT}`);
+// });
+
+//--- TAKE TWO ---//
+
+// app.set('view engine', 'handlebars')
+// app.engine('handlebars', handlebars({
+//   defaultView: 'default',
+//   layoutsDir: __dirname + '/views/layouts',
+//   partialsDir: __dirname + '/views/Partials'
+// }));
+
+// const express = require('express');
+// const router = express.Router();
+
+// router.get('/', function(req, res) {
+//   res.render('index')
+// })
+
+//--- TAKE THREE ---//
+
+var http = require('http')
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"})
+    response.end("Hello World\n")
+}).listen(process.env.PORT)
