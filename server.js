4457d37152026d80ee12cac1926ab19be0d14941
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-15 18:09:16 -0500

    more

--- server.js
+++ server.js
@@ -9,22 +9,19 @@
 //-- ^^^ PROBLEM AREA ^^^ -- //
 
 var express = require('express');
-var exphbs = require('express-handlebars')
+var exphbs = require('express-handlebars');
+
 var app = express();
 var PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.set('view engine', 'exphbs');
-app.engine('exphbs', exphbs({
-  defaultLayout: "main"
-}))
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-  res.render('index.handlebars', {layout: 'main'})
+  res.render('index');
 })
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + `http://localhost:${PORT}`);
-});
+app.listen(PORT);
