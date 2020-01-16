4e860942e211f11aa731a5f44cf99344c0eeafd2
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-15 19:24:57 -0500

    more

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-
-var app = express();
 var PORT = process.env.PORT || 8000;
 
+var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine('hbs', exphbs({
-  extname: '.handlebars',
-  defaultView: 'main',
-  layoutsDir: __dirname + 'views/layouts/',
-  partialsDir: __dirname + 'views/partials/'
-}));
-app.set('view engine', '.handlebars');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-  res.render((__dirname + '/views/index.handlebars'), {
-    layout: 'main'})
-})
+  res.render('index');
+});
 
 app.listen(PORT, function() {
   console.log(`Now listening on port ${PORT}`);
