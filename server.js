adda6b1d74ca69fc2349ec3c0674922b1d2f5006
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-16 07:34:20 -0500

    more

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
 var PORT = process.env.PORT || 8000;
 
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.send('testing\n');
 });
 
 app.listen(PORT, function() {
