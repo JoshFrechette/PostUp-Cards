26730652ffeac5bb004b1a59939cfc23e7e3b08e
Josh Frechette <josh.f.design@gmail.com>
2020-01-16 20:31:01 -0500

    Working version to create working JSON from API data for front-end. 2019 only with this version.

--- js/moment.js
+++ js/moment.js
@@ -1,5 +1,18 @@
 var moment = require('moment');
 
-define(['moment'], function (moment){
-console.log(moment().format('LLLL'));
-});
+// Code used to test to see if rational is working. Left in place to debug later if there's an issue.
+// let date = "2021-10-22T00:00:00.000Z";
+
+// var now = moment();
+// console.log(now);
+
+// function check(now, date) {
+//     if (now < moment(date)) {
+//         console.log("works")
+//     } else {
+//         console.log("shit")
+//     }
+// }
+// check(now, date);
+
+var now = moment();
