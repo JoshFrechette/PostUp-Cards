0ed5c487912aec9756e30c88510ab892249f0e49
shaili shah <sshaili2912@gmail.com>
2020-01-18 02:31:54 -0500

    with all Api & database

--- Public/assets/js/playerInfo.js
+++ Public/assets/js/playerInfo.js
@@ -1,8 +1,20 @@
 
 function playerInformation() {
   var player = $("#SearchPlayer").val();
-  NBAPlayerInfo(player);
+  NBAPlayerGif(player);
 };
+
+function NBAPlayerGif(player){
+  var gifs = "http://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=NOrybviPafhsVNwUf5V1x96NAM664s7W&limit=5";
+  $.ajax({
+              url: gifs,
+              method: "GET"
+              }).then(function(gifs) {
+                $("#card").append("<img src="+gifs.data[0].images.original.url+">");
+                //$("#playerID").text("Player's id: " + response.data[0].id);
+                NBAPlayerInfo(player);
+})
+}
 
 function NBAPlayerInfo(player){
   var ballDontLie = "https://cors-anywhere.herokuapp.com/https://balldontlie.io/api/v1/players?search=" + player;
@@ -29,52 +41,53 @@ function NBAPlayerInfo(player){
                 $("#playercity").text("Player's city: " + response.data[0].team.city);
                 
                   var playerID = response.data[0].id;
-
-                  var playerStatatstics = "https://cors-anywhere.herokuapp.com/https://balldontlie.io/api/v1/stats?player_ids[]=" + playerID + "&seasons[]=2019";
-                  $.ajax({
-                      url: playerStatatstics,
-                      method: "GET"
-                      }).then(function(playerStats) {
-                          console.log(playerStats);
-                      })
-
+                  playerStatastics(playerID);
+                  
               })
-              
+           }
 
-          }
+function playerStatastics(playerID){
+  var playerStatUrl = "https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + playerID;
+                  $.ajax({
+                      url: playerStatUrl,
+                      method: "GET"
+                      }).then(function(playerStatUrl) {
+                          console.log(playerStatUrl);
 
+                          $("#card").append("<div class=col id=playeStatsfgm></div>");
+                          $("#playeStatsfgm").text("fgm: " + playerStatUrl.data[0].fgm);
 
-   
-   
-   // $("#submit").click(function() {
-    //   console.log('inside');
-    //   var playerName = $("#SearchPlayer").val();
-    //   console.log(playerName);
-    // })  
-//       // Send the POST request.
-//       $.ajax("/api/burgers", {
-//         type: "POST",
-//         data: newBurger
-//       }).then(
-//         function() {
-//           console.log("created new burger");
-//           // Reload the page to get the updated list
-//           location.reload();
-//         }
-//       );
-//     });
-//     $(".devourButton").click((event)=>{
-//       console.log(event.target);
-  
-//       $.ajax("/api/burgers/"+event.target.value,{
-//         type:"PUT",
-//         data:{
-//           devoured:1
-//         }
-//       }).then(()=>{
-//         $(`.${event.target.className}`).remove();
-//         location.reload();
-//       })
-//     })
-//   });
+                          $("#card").append("<div class=col id=playeStatsfga></div>");
+                          $("#playeStatsfga").text("fga: " + playerStatUrl.data[0].fga);
 
+                          $("#card").append("<div class=col id=playeStatsfg3m></div>");
+                          $("#playeStatsfg3m").text("fg3m: " + playerStatUrl.data[0].fg3m);
+
+                          $("#card").append("<div class=col id=playeStatsfg3a></div>");
+                          $("#playeStatsfg3a").text("fg3a: " + playerStatUrl.data[0].fg3a);
+
+                          $("#card").append("<div class=col id=playeStatsftm></div>");
+                          $("#playeStatsftm").text("ftm: " + playerStatUrl.data[0].ftm);
+
+                          $("#card").append("<div class=col id=playeStatsfta></div>");
+                          $("#playeStatsfta").text("fta: " + playerStatUrl.data[0].fta);
+
+                          $("#card").append("<div class=col id=playeStatsoreb></div>");
+                          $("#playeStatsoreb").text("oreb: " + playerStatUrl.data[0].oreb);
+
+                          $("#card").append("<div class=col id=playeStatsdreb></div>");
+                          $("#playeStatsdreb").text("dreb: " + playerStatUrl.data[0].dreb);
+
+                          $("#card").append("<div class=col id=playeStatsast></div>");
+                          $("#playeStatsast").text("ast: " + playerStatUrl.data[0].ast);
+
+                          $("#card").append("<div class=col id=playeStatsstl></div>");
+                          $("#playeStatsstl").text("stl: " + playerStatUrl.data[0].stl);
+
+                          $("#card").append("<div class=col id=playeStatsblk></div>");
+                          $("#playeStatsblk").text("blk: " + playerStatUrl.data[0].blk);
+
+                          $("#card").append("<div class=col id=playeStatspts></div>");
+                          $("#playeStatspts").text("pts: " + playerStatUrl.data[0].pts);
+                      })
+                    }
