a05d6f8a7d086827849c7d7f4e29123eb6c79584
shaili shah <sshaili2912@gmail.com>
2020-01-21 13:28:05 -0500

    with clear function and card gui

--- Public/assets/js/playerInfo.js
+++ Public/assets/js/playerInfo.js
@@ -9,6 +9,7 @@ let multiple = 1;
 
 function playerInformation() {
     var player = $("#SearchPlayer").val();
+    $("#card").css('border', "solid 4px blue"); 
     NBAPlayerGif(player);
     NBAPlayerInfo(player);
 };
@@ -19,7 +20,7 @@ function NBAPlayerGif(player) {
         url: gifs,
         method: "GET"
     }).then(function (gifs) {
-        $("#card").append("<img src=" + gifs.data[0].images.original.url + ">");
+        $("#card-landscape").append("<img src=" + gifs.data[0].images.original.url + ">");
         //$("#playerID").text("Player's id: " + response.data[0].id);
         NBAPlayerInfo(player);
     })
@@ -31,12 +32,14 @@ function NBAPlayerInfo(player) {
         url: ballDontLie,
         method: "GET"
     }).then(function (response) {
+        $("#card").append("<div class=col id=playerName></div>");
+        $("#playerName").css({'font-family': "Impact,Charcoal,sans-serif", 'font-size': "xxx-large", 'text-align': 'center'});
+        $("#playerName").text(response.data[0].first_name + " " + response.data[0].last_name);
+
         $("#card").append("<div class=col id=playerID></div>");
         $("#playerID").text("Player's id: " + response.data[0].id);
 
-        $("#card").append("<div class=col id=playerName></div>");
-        $("#playerName").text("Player's name: " + response.data[0].first_name + " " + response.data[0].last_name);
-
+        
         $("#card").append("<div class=col id=playerHeight></div>");
         $("#playerHeight").text("Player's height: " + response.data[0].height_feet + " ft");
 
@@ -119,7 +122,7 @@ function playerStatastics(playerID) {
         }
         stats.push(plyrData);
 
-        $("#card-landscape").append("<div class=col id=playeStatsseason></div>");
+        $("#card").append("<div class=col id=playeStatsseason></div>");
         $("#playeStatsseason").text("Season: " + stats[0].Season + " FG_Made: " + stats[0].FG_Made + " FG_Att: " + stats[0].FG_Att + " Three_Pts_Made: " + stats[0].Three_Pts_Made + " Three_Pts_Att: " + stats[0].Three_Pts_Att +
          " FT Made: " + stats[0].FT_Made + " FT Att: " + stats[0].FT_Att + " Off Reb: " + stats[0].Off_Reb + " Def Reb: " + stats[0].Def_Reb + " Asst: " + stats[0].Asst + " Stl: " + stats[0].Stl + " Blk: " + stats[0].Blk + " Pts: " + stats[0].Pts + " AVG: " + stats[0].AVG );
 
@@ -213,7 +216,7 @@ let seasonStats = (currentSeason, playerID) => {
     }
     avgStats.push(plrAvgStats);
 
-    $("#card-landscape").append("<div class=col id=plyrAvgSeason></div>");
+    $("#card").append("<div class=col id=plyrAvgSeason></div>");
     $("#plyrAvgSeason").text(" Season: " + currentSeason + " FG Made AVG: " + seasonAvg.data[0].fgm + " FG Att AVG: " + seasonAvg.data[0].fga + " Three Pts Made AVG: " + seasonAvg.data[0].fg3m +
     " Three Pts Att AVG: " + seasonAvg.data[0].fg3a + " FT Made AVG: " + seasonAvg.data[0].ftm + " FT Att AVG: " + seasonAvg.data[0].fta + " Off Reb AVG: " + seasonAvg.data[0].oreb + " Def Reb AVG: " + seasonAvg.data[0].dreb
     + " Asst AVG: " + seasonAvg.data[0].ast + " Stl AVG: " + seasonAvg.data[0].stl + " Blk AVG: " + seasonAvg.data[0].blk + " Pts AVG: " + seasonAvg.data[0].pts);
