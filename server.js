317c5a92d09d428242fe41e78383169195698f30
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-01-20 22:34:48 -0500

    more handlebars formatting

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 
-// placeholder
 var express = require("express");
 var path = require("path");
-// Set Handlebars.
 const exphbs = require("express-handlebars");
 
-// Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory to be served
 app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
-// Routes
 
 app.get("/", function(req, res) {
   res.render(path.join(__dirname, "./views/layouts/welcome"));
@@ -25,6 +20,7 @@ app.get("/", function(req, res) {
 app.get("/welcome", function(req, res) {
   res.render(path.join(__dirname, "./views/layouts/welcome"));
 });
+
 app.get("/create", function(req, res) {
   res.render(path.join(__dirname, "./views/index"));
 });
@@ -37,15 +33,14 @@ app.get("/signup", function(req, res) {
   res.render(path.join(__dirname, "./views/layouts/signup"));
 });
 
-app.get("/deckview", function(req, res) {
-    res.render(path.join(__dirname, "./views/layouts/Deck"));
-  });
+app.get("/deck", function(req, res) {
+    res.render(path.join(__dirname, "./views/layouts/deck"));
+});
   
 app.get("/back", function(req, res) {
     res.render(path.join(__dirname, "./views/layouts/Back"));
-  });
-// Starts the server to begin listening
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + `http://localhost:${PORT}/welcome`);
-  console.log("this is working");
 });
